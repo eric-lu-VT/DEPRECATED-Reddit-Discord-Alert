e2c1171526446fe00636f17b8e6b4f632f389b85
11Some1 <68611574+11Some1@users.noreply.github.com>
2021-10-21 21:15:11 -0400

    Added more embd stuff

--- index.js
+++ index.js
@@ -43,14 +43,14 @@ function searchReddit(guildId) {
                         else {
                             new RedditPost({
                                 postid: listing.name,
-                                subreddit: encodeURI(listing.subreddit_name_prefixed),
-                                url: encodeURI("https://www.reddit.com" + listing.permalink),
+                                subreddit: (listing.subreddit_name_prefixed),
+                                url: ("https://www.reddit.com" + listing.permalink),
                                 date: listing.created_utc,
                                 guildId: guildId
                             }).save();
                             console.log("Made an entry to DB");
                             
-                            var title = encodeURI(listing.title);
+                            var title = (listing.title);
                             if(title.length > 253) {
                                 title = title.substring(0, 253) + "...";
                             }
@@ -61,9 +61,9 @@ function searchReddit(guildId) {
                                     .setAuthor(listing.author.name, "", encodeURI("https://www.reddit.com/u/" + listing.author.name))
                                     .setTitle(title)
                                     .setDescription(listing.score + " votes and " + listing.num_comments + " comments so far")
-                                    .setFooter("On " + encodeURI(listing.subreddit_name_prefixed))
+                                    .setFooter("On " + listing.subreddit_name_prefixed)
                                     .setTimestamp(listing.created * 1000)
-                                    .setURL("https://www.reddit.com" + encodeURI(listing.permalink))]});
+                                    .setURL("https://www.reddit.com" + listing.permalink)]});
                             });
                         }
                     });
@@ -78,8 +78,7 @@ function registerCommands() {
         const rest = new REST({ version: '9' }).setToken(discordBotToken);
         doc.forEach(res => {
             rest.put(Routes.applicationGuildCommands(discordId, res._id), { body: commands })
-                .then(() => console.log('Successfully registered application commands.'))
-                .catch(console.error);
+                .then().catch(console.error);
             });
     });
 }
@@ -98,43 +97,75 @@ client.on('interactionCreate', async interaction => {
 		await interaction.reply('Pong!');
     } 
     else if(commandName === 'addchannel') {
-        ServerPost.exists({_id: interaction.guildId}).then((result) => {
-            if(result) {
-                ServerPost.findOneAndUpdate({_id: interaction.guildId}, 
-                                            { $push : { 'channels': interaction.channelId}}, 
-                                            {upsert: true}).then(data => {});
-            }
-            else {
-                new ServerPost({
-                    _id: interaction.guildId,
-                    channels: [interaction.channelId],
-                    queries: [{
-                        query: "Henry",
-                        subreddit: "NFL"
-                    }]
-                }).save();
-            }
-        });
-        await interaction.reply("Added channel " + interaction.channelId + " in server " + interaction.guildId);
+        ServerPost.findOneAndUpdate({_id: interaction.guildId}, 
+                                    { $push : { 'channels': interaction.channelId}}, 
+                                    {upsert: true}).then(data => {});
+            
+        var embd = new MessageEmbed()
+            .setColor([46, 204, 113])
+            .setTitle("Added channel!")
+            .setDescription("Added the following channel:")
+            .addField("Channel", String(interaction.channelId))
+            .addField("Server", String(interaction.guildId))
+            .setAuthor(interaction.user.username, 
+                `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.png?size=256`)
+            .setTimestamp();    
+        await interaction.reply({embeds : [embd]});
     } 
     else if (commandName === 'removechannel') {
         ServerPost.findOneAndUpdate({_id: interaction.guildId}, 
             { $pull : { 'channels': interaction.channelId}}).then(data => {});
-        await interaction.reply("Removed channel " + interaction.channelId + " in server " + interaction.guildId);
+
+        var embd = new MessageEmbed()
+            .setColor([46, 204, 113])
+            .setTitle("Removed channel!")
+            .setDescription("Removed the following channel:")
+            .addField("Channel", String(interaction.channelId))
+            .addField("Server", String(interaction.guildId))
+            .setAuthor(interaction.user.username, 
+                `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.png?size=256`)
+            .setTimestamp();
+        await interaction.reply({embeds : [embd]});
     }
     else if (commandName === 'start') {
         setInterval(function(){searchReddit(interaction.guildId);}, 30000);
-        await interaction.reply("Starting search loop");
+
+        var embd = new MessageEmbed()
+            .setColor([46, 204, 113])
+            .setTitle("Starting search!")
+            .setDescription("Now starting the search loop...")
+            .addField("Server", String(interaction.guildId))
+            .setAuthor(interaction.user.username, 
+                `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.png?size=256`)
+            .setTimestamp();
+        ServerPost.findOne({ _id: interaction.guildId}, ['channels']).then((resdoc) => { 
+            resdoc.channels.forEach(c => {
+                embd.addField("Channel", String(c));
+            });
+            interaction.reply({embeds : [embd]});
+        });
     } 
     else if (commandName === 'stop') {
         clearInterval(function(){searchReddit(interaction.guildId);});
-        await interaction.reply("Stopped search loop");
+        var embd = new MessageEmbed()
+            .setColor([46, 204, 113])
+            .setTitle("Stopping search!")
+            .setDescription("Stopped the search loop in this server.")
+            .addField("Server", String(interaction.guildId))
+            .setAuthor(interaction.user.username, 
+                `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.png?size=256`)
+            .setTimestamp();
+        ServerPost.findOne({ _id: interaction.guildId}, ['channels']).then((resdoc) => { 
+            resdoc.channels.forEach(c => {
+                embd.addField("Channel", String(c));
+            });
+            interaction.reply({embeds : [embd]});
+        });
     } 
     else if (commandName === 'addquery') {
         query = interaction.options.getString("input").split(" ");
         var queryStr = "";
         var subredditStr = "";
-        
         if(query.length > 1) {
             if(query.length == 1) {
                 queryStr = query[0];
@@ -146,20 +177,40 @@ client.on('interactionCreate', async interaction => {
                 }
                 subredditStr = query[query.length - 1];
             }
-            ServerPost.findOneAndUpdate({_id: interaction.guildId}, 
-                { $push : { 'queries': {
-                    query: queryStr,
-                    subreddit: subredditStr
-                }}}, 
-                {upsert: true}).then(data => {});
-            await interaction.reply('Added search query:\n{ query: ' + queryStr + ',\nsubreddit: ' + subredditStr + ' }');
         }
+        queryStr = queryStr.toLowerCase();
+        subredditStr = subredditStr.toLowerCase();
+
+        var embd = new MessageEmbed()
+            .setColor([231, 76, 60])
+            .setTitle("Failed to add query...")
+            .setDescription("The following query already exists:")
+            .addField("Query", String(queryStr))
+            .addField("Subreddit", String(subredditStr))
+            .setAuthor(interaction.user.username, 
+                `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.png?size=256`)
+            .setTimestamp();
+
+        ServerPost.exists({queries: {$elemMatch: { query: queryStr, subreddit: subredditStr}}})
+        .then((result) => {
+            if(!result) {
+                ServerPost.findOneAndUpdate({_id: interaction.guildId}, 
+                    { $push : { 'queries': {
+                        query: queryStr,
+                        subreddit: subredditStr
+                    }}}, 
+                    {upsert: true}).then(data => {});
+                embd.setColor([46, 204, 113]);
+                embd.setTitle("Added query!");
+                embd.setDescription("Added the following query:");
+            }
+            interaction.reply({embeds : [embd]});
+        })
     } 
     else if (commandName === 'removequery') {
         query = interaction.options.getString("input").split(" ");
         var queryStr = "";
         var subredditStr = "";
-        
         if(query.length > 1) {
             if(query.length == 1) {
                 queryStr = query[0];
@@ -171,14 +222,34 @@ client.on('interactionCreate', async interaction => {
                 }
                 subredditStr = query[query.length - 1];
             }
-            ServerPost.findOneAndUpdate({_id: interaction.guildId}, 
-                { $pull : { 'queries': {
-                    query: queryStr,
-                    subreddit: subredditStr
-                }}}).then(data => {});
-            
-            await interaction.reply('Removed search query:\n{ query: ' + queryStr + ',\nsubreddit: ' + subredditStr + ' }');
         }
+        queryStr = queryStr.toLowerCase();
+        subredditStr =subredditStr.toLowerCase();
+
+        var embd = new MessageEmbed()
+            .setColor([231, 76, 60])
+            .setTitle("Failed to remove query...")
+            .setDescription("The following query does not exist in the database:")
+            .addField("Query", String(queryStr))
+            .addField("Subreddit", String(subredditStr))
+            .setAuthor(interaction.user.username, 
+                `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.png?size=256`)
+            .setTimestamp();
+
+        ServerPost.exists({queries: {$elemMatch: { query: queryStr, subreddit: subredditStr}}})
+        .then((result) => {
+            if(result) {
+                ServerPost.findOneAndUpdate({_id: interaction.guildId}, 
+                    { $pull : { 'queries': {
+                        query: queryStr,
+                        subreddit: subredditStr
+                    }}}).then(data => {});
+                embd.setColor([46, 204, 113]);
+                embd.setTitle("Removed query!");
+                embd.setDescription("Removed the following query:");
+            }
+            interaction.reply({embeds : [embd]});
+        })
     }
 });
 
@@ -200,12 +271,6 @@ client.on("guildCreate", guild => {
             subreddit: "NFL"
         }]
     }).save();
-    // Remove dummy entry in queries
-    ServerPost.findOneAndUpdate({_id: guild.id}, 
-        { $pull : { 'queries': {
-            query: "Henry",
-            subreddit: "NFL"
-        }}}).then(data => {});
 
     registerCommands();
 });
@@ -215,4 +280,3 @@ client.on("guildDelete", guild => {
 });
 
 client.login(discordBotToken);
-
