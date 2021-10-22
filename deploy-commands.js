ee199275870a0a38d3e3d69eb44de4e4d0174e42
11Some1 <68611574+11Some1@users.noreply.github.com>
2021-10-21 21:36:51 -0400

    Updated some command display text

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,10 +2,10 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('addchannel').setDescription('Allows the bot to post in the current channel'),
-	new SlashCommandBuilder().setName('removechannel').setDescription('Revokes the bot\'s access to post in the current channel'),
-	new SlashCommandBuilder().setName('start').setDescription('Starts the looping search query'),
-	new SlashCommandBuilder().setName('stop').setDescription('Stops the looping search query'),
+	new SlashCommandBuilder().setName('addchannel').setDescription('Allows the bot to post in the channel in which the command was sent.'),
+	new SlashCommandBuilder().setName('removechannel').setDescription('Revokes the bot\'s access to post in the channel in which the command was sent.'),
+	new SlashCommandBuilder().setName('start').setDescription('Starts the looping search query in the server.'),
+	new SlashCommandBuilder().setName('stop').setDescription('Stops the looping search query in the server.'),
 	new SlashCommandBuilder().setName('addquery').setDescription('Adds a new query to the search list')
 		.addStringOption(option => 
 			option.setName('input')
